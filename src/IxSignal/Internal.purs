c0803a1328faf6d551fd1d818febe34da0d4bde2
Athan Clark <athan.clark@gmail.com>
2017-11-30 14:16:56 -0700

    individual settings

--- src/IxSignal/Internal.purs
+++ src/IxSignal/Internal.purs
@@ -2,6 +2,7 @@ module IxSignal.Internal where
 
 import Prelude hiding (map)
 import Data.Tuple (Tuple (..))
+import Data.Maybe (Maybe (..))
 import Data.Traversable (traverse_)
 import Data.StrMap (StrMap)
 import Data.StrMap as StrMap
@@ -13,7 +14,8 @@ import Control.Monad.Eff.Ref (REF, Ref, modifyRef, newRef, readRef, writeRef)
 
 newtype IxSignal eff a = IxSignal
   { subscribers :: Ref (StrMap (String -> a -> Eff eff Unit))
-  , value :: Ref a
+  , individual :: Ref (StrMap a)
+  , broadcast :: Ref a
   }
 
 subscribe :: forall eff a
@@ -43,44 +45,90 @@ subscribeIxWithKey :: forall eff a
                   -> String
                   -> IxSignal (ref :: REF | eff) a
                   -> Eff (ref :: REF | eff) Unit
-subscribeIxWithKey f k (IxSignal {subscribers,value}) = do
-  x <- readRef value
+subscribeIxWithKey f k (IxSignal {subscribers,individual,broadcast}) = do
+  x <- do
+    mI <- StrMap.lookup k <$> readRef individual
+    case mI of
+      Nothing -> readRef broadcast
+      Just i -> do
+        modifyRef individual (StrMap.delete k)
+        pure i
   f k x
-  modifyRef subscribers (\xs -> StrMap.insert k f xs)
+  modifyRef subscribers (StrMap.insert k f)
 
 -- | Publish a message to the set of subscribers
 set :: forall eff a. a -> IxSignal (ref :: REF | eff) a -> Eff (ref :: REF | eff) Unit
-set x (IxSignal {subscribers,value}) = do
+set x (IxSignal {subscribers,individual,broadcast}) = do
   fs <- readRef subscribers
   traverse_ (\(Tuple k f) -> f k x) (StrMap.toUnfoldable fs :: Array (Tuple String (String -> a -> Eff (ref :: REF | eff) Unit)))
-  writeRef value x
+  writeRef broadcast x
 
 setIx :: forall eff a. a -> String -> IxSignal (ref :: REF | eff) a -> Eff (ref :: REF | eff) Unit
-setIx x k (IxSignal {subscribers,value}) = do
+setIx x k (IxSignal {subscribers,individual,broadcast}) = do
   mF <- StrMap.lookup k <$> readRef subscribers
-  traverse_ (\f -> f k x) mF
-  writeRef value x
+  case mF of
+    Nothing -> modifyRef individual (StrMap.insert k x)
+    Just f -> do
+      f k x
+      modifyRef individual (StrMap.delete k)
 
 -- | Gets the last message published to the subscribers
 get :: forall eff a. IxSignal (ref :: REF | eff) a -> Eff (ref :: REF | eff) a
-get (IxSignal {value}) = readRef value
+get (IxSignal {broadcast}) = readRef broadcast
+
+
+getIx :: forall eff a. String -> IxSignal (ref :: REF | eff) a -> Eff (ref :: REF | eff) a
+getIx k (IxSignal {individual,broadcast}) = do
+  mX <- StrMap.lookup k <$> readRef individual
+  case mX of
+    Nothing -> readRef broadcast
+    Just x -> pure x
+
 
 -- | Removes all subscribers
+clearSubscribers :: forall eff a
+       . IxSignal (ref :: REF | eff) a
+      -> Eff (ref :: REF | eff) Unit
+clearSubscribers (IxSignal {subscribers}) =
+  writeRef subscribers StrMap.empty
+
+-- | Removes all individual
+clearIndividual :: forall eff a
+       . IxSignal (ref :: REF | eff) a
+      -> Eff (ref :: REF | eff) Unit
+clearIndividual (IxSignal {individual}) =
+  writeRef individual StrMap.empty
+
+
+-- | Removes all from both
 clear :: forall eff a
        . IxSignal (ref :: REF | eff) a
       -> Eff (ref :: REF | eff) Unit
-clear (IxSignal {subscribers}) =
-  writeRef subscribers StrMap.empty
+clear sig = clearSubscribers sig >>= \_ -> clearIndividual sig
+
+
+deleteSubscriber :: forall eff a
+        . String -> IxSignal (ref :: REF | eff) a
+       -> Eff (ref :: REF | eff) Unit
+deleteSubscriber k (IxSignal {subscribers}) =
+  modifyRef subscribers (StrMap.delete k)
+
+deleteIndividual :: forall eff a
+        . String -> IxSignal (ref :: REF | eff) a
+       -> Eff (ref :: REF | eff) Unit
+deleteIndividual k (IxSignal {individual}) =
+  modifyRef individual (StrMap.delete k)
 
 delete :: forall eff a
         . String -> IxSignal (ref :: REF | eff) a
        -> Eff (ref :: REF | eff) Unit
-delete k (IxSignal {subscribers}) =
-  modifyRef subscribers (StrMap.delete k)
+delete k sig = deleteSubscriber k sig >>= \_ -> deleteIndividual k sig
+
 
 -- | Create a signal with a starting value
 make :: forall eff a. a -> Eff (ref :: REF | eff) (IxSignal (ref :: REF | eff) a)
 make x = do
   subscribers <- newRef StrMap.empty
-  value <- newRef x
-  pure (IxSignal {subscribers,value})
+  broadcast <- newRef x
+  individual <- newRef StrMap.empty
+  pure (IxSignal {subscribers,individual,broadcast})
