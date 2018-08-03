3edb71cf69f6c45acd5fbfca93ad2d45cd7dd412
Athan Clark <athan.clark@gmail.com>
2018-08-02 21:04:44 -0600

    scope

---
+++ src/Signal/Types.purs
@@ -0,0 +1,20 @@
+module Signal.Types where
+
+import Prelude (Unit)
+import Control.Monad.Eff (Eff, kind Effect)
+
+
+foreign import kind SCOPE
+
+foreign import data WRITE :: SCOPE
+
+foreign import data READ :: SCOPE
+
+type Handler eff a = a -> Eff eff Unit
+
+
+class SignalScope (s :: # SCOPE -> # Effect -> Type -> Type) where
+  readOnly      :: forall rw eff a. s (read  :: READ  | rw) eff a -> s (read  :: READ)  eff a
+  writeOnly     :: forall rw eff a. s (write :: WRITE | rw) eff a -> s (write :: WRITE) eff a
+  allowReading  :: forall rw eff a. s (read  :: READ)  eff a -> s (read  :: READ  | rw) eff a
+  allowWriteing :: forall rw eff a. s (write :: WRITE) eff a -> s (write :: WRITE | rw) eff a
